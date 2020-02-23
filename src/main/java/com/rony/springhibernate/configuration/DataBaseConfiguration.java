25d463c703fd63b772f4ec948fed013b43551e2d
RonyNic99 <ronydiaz99@gmail.com>
2020-02-23 00:19:11 -0600

    Primer Commit Integracion de la base de datos a travès de la clase DataBaseConfiguration

---
+++ src/main/java/com/rony/springhibernate/configuration/DataBaseConfiguration.java
@@ -0,0 +1,39 @@
+package com.rony.springhibernate.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.sql.DriverManager;
+import java.util.Properties;
+
+@Configuration
+@EnableTransactionManagement
+public class DataBaseConfiguration {
+    @Bean
+    public LocalSessionFactoryBean sessionFactoryBean(){
+        LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
+        sessionFactoryBean.setDataSource(dataSource());
+        sessionFactoryBean.setPackagesToScan("com.rony.springhibernate.model");
+        sessionFactoryBean.setHibernateProperties(hibernateProperties());
+        return sessionFactoryBean;
+    }
+    @Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.mariadb.jdbc.Driver");
+        dataSource.setUrl("jdbc:mariadb://localhost:3306/ronyprofesores");
+        dataSource.setUsername("root");
+        dataSource.setPassword("12345678");
+        return dataSource;
+    }
+    public Properties hibernateProperties(){
+        Properties properties = new Properties();
+        properties.put("hibernate.dialect","org.hibernate.dialect.MariaDB53Dialect");
+        properties.put("hibernate.show_sql",true);
+        return properties;
+    }
+}
